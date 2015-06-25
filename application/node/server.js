431dd24ee6808aaeed4c0523746d5855f4453595
Barry <barry.buck@gloo.co.za>
2015-06-25 02:27:43 +0200

    minor ignore update

--- application/node/server.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  console.log(req.url);
-  res.writeHead(200, {'Content-Type': 'text/plain', 'Content-Type': 'application/json;charset=utf-8'});
-  res.end('Hello Nodejs!');
-}).listen(8080, "0.0.0.0");
-console.log('Server running at http://0.0.0.0:8080/');
